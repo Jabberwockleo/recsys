1707028721d837a5b82554b30ecfcc637815013b
Leo Wan <jabberwockwanli@gmail.com>
2019-08-07 16:32:53 +0800

    dev

--- samplers/feeds_sampler.py
+++ samplers/feeds_sampler.py
@@ -44,7 +44,7 @@ def create_training_sampler(dataset, featurizer, max_pos_neg_per_user=5, num_pro
             Batcher function
             Returns: ndarray shaped (2*batch_size,)
                 typed [
-                ('user_demography_vec', (np.float32, featurizer.fea_user_demography_dim())), 
+                ('user_demography_vec', (np.float32, featurizer.fea_user_demography_dim())),
                 ('user_stat_vec', (np.float32, featurizer.fea_user_stat_dim())),
                 ('user_history_vec', (np.float32, featurizer.fea_user_history_dim())),
                 ('user_history_len', np.int32)
@@ -72,12 +72,14 @@ def create_training_sampler(dataset, featurizer, max_pos_neg_per_user=5, num_pro
             user_id = random.sample(warm_users, 1)[0]
             # get positive items
             pos_items = dataset.get_positive_items(user_id, sort=False)
-            pos_items = random.sample(pos_items, max_pos_neg_per_user)
+            pos_sample_num = min(len(pos_items), max_pos_neg_per_user)
+            pos_items = random.sample(pos_items, pos_sample_num)
             if len(pos_items) == 0:
                 continue
             # get negative items
             neg_items = dataset.get_negative_items(user_id)
-            neg_items = random.sample(neg_items, max_pos_neg_per_user)
+            neg_sample_num = min(len(neg_items), max_pos_neg_per_user)
+            neg_items = random.sample(neg_items, neg_sample_num)
             if len(neg_items) == 0:
                 continue
             for pos_item in pos_items:
@@ -85,16 +87,16 @@ def create_training_sampler(dataset, featurizer, max_pos_neg_per_user=5, num_pro
                     pos_dict = featurizer.featurize(user_id, pos_item)
                     neg_dict = featurizer.featurize(user_id, neg_item)
                     sample = np.zeros(2, dtype=[
-                        ('user_demography_vec', (np.float32, fea_user_demography_dim)), 
+                        ('user_demography_vec', (np.float32, fea_user_demography_dim)),
                         ('user_stat_vec', (np.float32, fea_user_stat_dim)),
                         ('user_history_vec', (np.float32, fea_user_history_dim)),
-                        ('user_history_len', np.int32)
+                        ('user_history_len', np.int32),
                         ('item_meta_vec_1', (np.float32, fea_item_meta_dim)),
                         ('item_stat_vec_1', (np.float32, fea_item_stat_dim)),
-                        ('item_id_1', np.int32)
+                        ('item_id_1', np.int32),
                         ('item_meta_vec_2', (np.float32, fea_item_meta_dim)),
                         ('item_stat_vec_2', (np.float32, fea_item_stat_dim)),
-                        ('item_id_2', np.int32)
+                        ('item_id_2', np.int32),
                         ('context_hour', (np.float32, fea_context_hour_dim)),
                         ('label', np.int32)])
                     sample[0] = (
@@ -128,13 +130,13 @@ def create_training_sampler(dataset, featurizer, max_pos_neg_per_user=5, num_pro
                     else:
                         input_data = np.hstack([input_data, sample])
                     sample_cnt += 1
-                    if sample_cnt == batch_size:                        
+                    if sample_cnt == batch_size:
                         yield input_data # (2*batch_size,) per iteration
                         input_data = None
                         sample_cnt = 0
         raise "unreachable!"
     s = Sampler(dataset=dataset, generate_batch=batch, evaluation_type="SAMPLED", featurizer=featurizer, num_process=num_process)
-    
+
     return s
 
 
@@ -172,7 +174,7 @@ def create_evaluation_sampler(dataset, featurizer, max_pos_neg_per_user=20, seed
                 labels: array of labels, 1 for positive item, -1 for negative item. Marks for later rank score classification
                 feature_vecs: ndarray shaped (2*batch_size,)
                 typed [
-                ('user_demography_vec', (np.float32, featurizer.fea_user_demography_dim())), 
+                ('user_demography_vec', (np.float32, featurizer.fea_user_demography_dim())),
                 ('user_stat_vec', (np.float32, featurizer.fea_user_stat_dim())),
                 ('user_history_vec', (np.float32, featurizer.fea_user_history_dim())),
                 ('user_history_len', np.int32)
@@ -182,30 +184,38 @@ def create_evaluation_sampler(dataset, featurizer, max_pos_neg_per_user=20, seed
                 ('context_hour', (np.float32, featurizer.fea_context_hour_dim()))]
             assume n positive samples, m negative samples for each user, ? = m+n per user, (1,) per iteration
         """
-        dim = featurizer.feature_dim()
+        fea_user_demography_dim = featurizer.fea_user_demography_dim()
+        fea_user_stat_dim = featurizer.fea_user_stat_dim()
+        fea_user_history_dim = featurizer.fea_user_history_dim()
+        fea_item_meta_dim = featurizer.fea_item_meta_dim()
+        fea_item_stat_dim = featurizer.fea_item_stat_dim()
+        fea_context_hour_dim = featurizer.fea_context_hour_dim()
+
         while True:
             for user_id in dataset.warm_users(threshold=max_pos_neg_per_user):
                 # get positive items
                 pos_items = dataset.get_positive_items(user_id, sort=False)
-                pos_items = random.sample(pos_items, max_pos_neg_per_user)
+                pos_sample_num = min(len(pos_items), max_pos_neg_per_user)
+                pos_items = random.sample(pos_items, pos_sample_num)
                 if len(pos_items) == 0:
                     continue
                 # get negative items
                 neg_items = dataset.get_negative_items(user_id)
-                neg_items = random.sample(neg_items, max_pos_neg_per_user)
+                neg_sample_num = min(len(neg_items), max_pos_neg_per_user)
+                neg_items = random.sample(neg_items, neg_sample_num)
                 if len(neg_items) == 0:
                     continue
                 labels = []
                 input_data = None
                 for pos_item in pos_items:
                     sample = np.zeros(1, dtype=[
-                        ('user_demography_vec', (np.float32, fea_user_demography_dim)), 
+                        ('user_demography_vec', (np.float32, fea_user_demography_dim)),
                         ('user_stat_vec', (np.float32, fea_user_stat_dim)),
                         ('user_history_vec', (np.float32, fea_user_history_dim)),
-                        ('user_history_len', np.int32)
+                        ('user_history_len', np.int32),
                         ('item_meta_vec', (np.float32, fea_item_meta_dim)),
                         ('item_stat_vec', (np.float32, fea_item_stat_dim)),
-                        ('item_id', np.int32)
+                        ('item_id', np.int32),
                         ('context_hour', (np.float32, fea_context_hour_dim))])
                     fea_dict = featurizer.featurize(user_id, pos_item)
                     sample[0] = (
@@ -224,7 +234,7 @@ def create_evaluation_sampler(dataset, featurizer, max_pos_neg_per_user=20, seed
                     labels.append(1)
                 for neg_item in neg_items:
                     sample = np.zeros(1, dtype=[
-                        ('user_demography_vec', (np.float32, fea_user_demography_dim)), 
+                        ('user_demography_vec', (np.float32, fea_user_demography_dim)),
                         ('user_stat_vec', (np.float32, fea_user_stat_dim)),
                         ('user_history_vec', (np.float32, fea_user_history_dim)),
                         ('user_history_len', np.int32)
